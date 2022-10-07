983c1db375adbbe3fbe00ea5f90a2251677899d5
brunoBrizo <bbrizolara7@gmail.com>
2022-10-06 21:23:30 -0300

    Add product table & lambda function

--- lib/aws-ecommerce-microservices-stack.ts
+++ lib/aws-ecommerce-microservices-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsEcommerceMicroservicesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AwsEcommerceMicroservicesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY, // only for Develop!
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsEcommerceMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const functionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_14_X
+    }
+    
+    const productFunction = new NodejsFunction(this, 'productFunction', {
+      entry: join(__dirname, `/../src/product/index.js`),
+      ...functionProps
+    });
+
+    // Grant permitions
+    productTable.grantReadWriteData(productFunction);
   }
 }
